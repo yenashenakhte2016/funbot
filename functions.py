1a2bc670868b54e4892c9a0718a85f2eb8e4f95e
Electrovirus <E.electrovirus@gmail.com>
2016-08-21 01:25:43 +0430

    Update functions.py

--- functions.py
+++ functions.py
@@ -11,7 +11,10 @@ from os.path import exists
 import os
 import re
 import logging
+import telebot
 from telebot import types
+import datetime
+from telegramcalendar import create_calendar
 import calendar
 reload(sys)
 sys.setdefaultencoding("utf-8")
@@ -186,6 +189,64 @@ def command_ayuda(m):
 def command_creator(m): 
     cid = m.chat.id 
     bot.send_message( cid, '🔵Squidward V.1 by @Electrovirus')
+
+@bot.message_handler(commands=['calendar'])
+def get_calendar(message):
+    now = datetime.datetime.now() #Current date
+    chat_id = message.chat.id
+    date = (now.year,now.month)
+    current_shown_dates[chat_id] = date #Saving the current date in a dict
+    markup= create_calendar(now.year,now.month)
+    bot.send_message(message.chat.id, "Please, choose a date", reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call: call.data[0:13] == 'calendar-day-')
+def get_day(call):
+    chat_id = call.message.chat.id
+    saved_date = current_shown_dates.get(chat_id)
+    if(saved_date is not None):
+        day=call.data[13:]
+        date = datetime.datetime(int(saved_date[0]),int(saved_date[1]),int(day),0,0,0)
+        bot.send_message(chat_id, str(date))
+        bot.answer_callback_query(call.id, text="")
+
+    else:
+        #Do something to inform of the error
+        pass
+
+@bot.callback_query_handler(func=lambda call: call.data == 'next-month')
+def next_month(call):
+    chat_id = call.message.chat.id
+    saved_date = current_shown_dates.get(chat_id)
+    if(saved_date is not None):
+        year,month = saved_date
+        month+=1
+        if month>12:
+            month=1
+            year+=1
+        date = (year,month)
+        current_shown_dates[chat_id] = date
+        markup= create_calendar(year,month)
+        bot.edit_message_text("Please, choose a date", call.from_user.id, call.message.message_id, reply_markup=markup)
+        bot.answer_callback_query(call.id, text="")
+    else:
+        #Do something to inform of the error
+        pass
+
+@bot.callback_query_handler(func=lambda call: call.data == 'previous-month')
+def previous_month(call):
+    chat_id = call.message.chat.id
+    saved_date = current_shown_dates.get(chat_id)
+    if(saved_date is not None):
+        year,month = saved_date
+        month-=1
+        if month<1:
+            month=12
+            year-=1
+        date = (year,month)
+        current_shown_dates[chat_id] = date
+        markup= create_calendar(year,month)
+        bot.edit_message_text("Please, choose a date", call.from_user.id, call.message.message_id, reply_markup=markup)
+        bot.answer_callback_query(call.id, text="")
 
 API_URL="http://127.0.0.1:5000/"
 
