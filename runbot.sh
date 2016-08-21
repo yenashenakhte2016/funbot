40a710c5b228ca7afbcdfea2677c47b263ec5516
Electrovirus <E.electrovirus@gmail.com>
2016-08-21 17:05:19 +0430

    Update runbot.sh

--- runbot.sh
+++ runbot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ########################################################################
-You can edit the path to select the correct .py file for run the script
+#You can edit the path to select the correct .py file for run the script
 ########################################################################
 
 python squidward.py
